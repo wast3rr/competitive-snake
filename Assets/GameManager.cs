088b99252f4cceea850282315b95f4ada8b3328d
Karen <midasg91@gmail.com>
2024-10-27 13:51:29 -0400

    Minigame launcher stuff

---
+++ Assets/GameManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using PlayerResult = SaveData.GameResult.PlayerResult;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private MiniGameManager _miniGameManager;
+    [SerializeField]
+    private GameObject _playerPrefab;
+    [SerializeField]
+    private Transform[] _spawnPoints;
+    private float _timer;
+    [SerializeField]
+    private float _gameDuration;
+    private bool _gameStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _miniGameManager.GameStarted += OnGameStarted;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_gameStarted)
+        {
+            return;
+        }
+
+        _timer -= Time.deltaTime;
+
+        if (_timer < 0)
+        {
+            EndGame();
+        }
+    }
+
+    void OnGameStarted(PlayerData[] playerDataArray)
+    {
+        Debug.Log(playerDataArray);
+        _gameStarted = true;
+        _timer = _gameDuration;
+
+        for (int i = 0; i < playerDataArray.Length; i++)
+        {
+            PlayerController playerInstance = Instantiate(_playerPrefab).GetComponent<PlayerController>();
+            playerInstance.Construct(playerDataArray[i]);
+            playerInstance.transform.position = _spawnPoints[i].position;
+        }
+    }
+
+    void EndGame()
+    {
+        PlayerResult[] results = new PlayerResult[] {
+            new PlayerResult(){Player=0, Points=100},
+            new PlayerResult(){Player=1, Points=50},
+            new PlayerResult(){Player=2, Points=25}
+            };
+        _miniGameManager.EndGame();
+        _gameStarted = false;
+    }
+}
--- Assets/SnakeMovementScript.cs
+++ Assets/SnakeMovementScript.cs
@@ -7,12 +7,17 @@ public class SnakeMovementScript : MonoBehaviour
     public Rigidbody2D snakebody;
     public float snakeSpeed = 2;
     public GameObject circle;
+    private List<Transform> segments = new List<Transform>();
+
     // Start is called before the first frame update
     void Start()
     {
         var newcircle = Instantiate(circle, new Vector3(transform.position.x, transform.position.y, 0), transform.rotation);
         newcircle.transform.parent = GameObject.Find("Snake").transform;
         snakebody.velocity = Vector2.right * snakeSpeed;
+
+        segments = new List<Transform>();
+        segments.Add(this.transform);
     }
 
     // Update is called once per frame
@@ -26,6 +31,21 @@ public class SnakeMovementScript : MonoBehaviour
             snakebody.velocity = Vector2.right * snakeSpeed;
         } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
             snakebody.velocity = Vector2.left * snakeSpeed;
+        }
+    }
+
+    private void Grow() {
+        Debug.Log("Test");
+        // Transform segment = Instantiate(snakebody);
+        // segment.position = segments[segments.Count - 1].position;
+        // segments.Add(segment);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            Grow();
         }
     }
 }
